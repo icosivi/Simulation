53580c2e08fcc477121bdc39c802b948c571ff68
federico <federico.siviero@edu.unito.it>
2019-01-19 19:22:17 +0100

    Tutto OK, manca solo la dose

--- Compilatore.C
+++ Compilatore.C
@@ -1,3 +1,25 @@
+#include <Riostream.h>
+#include <TObject.h>
+#include <TSystem.h>
+#include "TStopwatch.h"
+#include "TMath.h"
+#include "TAxis.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include <TTree.h>
+#include <TBranch.h>
+#include "TClonesArray.h"
+#include "TRandom3.h"
+#include "Punto.h"
+#include "Retta.h"
+#include "Neutron.h"
+#include "Propagatore.h"
+#include "Generatore.h"
+#include "Rivelatore.h"
+
+
 void Compilatore(){
   gSystem->CompileMacro("Punto.cxx","kgf");
   gSystem->CompileMacro("Retta.cxx","kgf");
